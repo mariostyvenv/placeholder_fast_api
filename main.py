272a242b788ef8b245062cb9ce350f76bdb8daeb
mario.velandia <mario.velandia@arus.com.co>
2023-04-14 13:54:05 -0500

    add health_check

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/api/placeholder")
+def hello():
     return {"Nombre: ": "<Su nombre>"}
+
+@app.get("/api/placeholder/health_check")
+def health_check():
+    return {"status: ": "ok"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
